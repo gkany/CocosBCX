681ce3557952bba47477c281770c9b7a37dc76ff
gkany <rootkangxu@yeah.net>
2020-03-23 19:23:59 +0800

    add contract example file

---
+++ contracts_api/contract_22_update_collateral_for_gas.lua
@@ -0,0 +1,4 @@
+function test_colllateral_gas(to, amount)
+    chainhelper:log("[test_colllateral_gas] to: " .. to .. ", amount: " .. tostring(amount))
+    chainhelper:update_collateral_for_gas('', to, amount)
+end
